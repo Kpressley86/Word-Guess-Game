5224faf591cb60e7e87d64df75d71f6fc9ef8ca4
Kelly Pressley <kellypressley86@gmail.com>
2019-08-22 16:27:53 -0700

    made an array of dog names

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var puppies = [ "Labrador Retriever", "German Shepherd", "Poodle", "Chihuahua", 
+"Golden Retriever", "Yorkshire Terrier", "Dachshund", "Beagle", "Boxer", 
+"Miniature Schnauzer", "Shih Tzu", "Bulldog", "Akita", "Cocker Spaniel", "St Bernard", 
+"French Bulldog", "Pug", "Rottweiler", "English Setter", "Maltese", "Basset Hound", 
+"German Shorthaired Pointer", "Weimaraner", "Border Collie",
+"Shetland Sheepdog", "Dobermann", "Chesapeake Bay Retriever", "Bernese Mountain", 
+"Great Dane", "Rhodesian Ridgeback", "Siberian Husky", "Australian Shepherd", "Corgi", 
+"Boston Terrier", "Pomeranian", "Havanese", "English Mastiff" ]
+
+
+
+
+
+
+console.log ()
