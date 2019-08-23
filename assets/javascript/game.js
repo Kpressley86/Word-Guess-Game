94f6f495a9e3e58ecd83a2a364b503e3286be74c
Kelly Pressley <kellypressley86@gmail.com>
2019-08-22 22:29:58 -0700

    starting begining JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,52 @@
+//Start//
+var name = prompt("What is your name?");
+console.log("Hello" + name);
+
 // Array of dog names //
 
-var puppyArray = [ "Labrador Retriever", "German Shepherd", "Poodle", "Chihuahua", 
-"Golden Retriever", "Yorkie", "Dachshund", "Beagle", "Boxer", 
-"Schnauzer", "Shih Tzu", "Bulldog", "Akita", "Cocker Spaniel", "St Bernard", 
-"Bulldog", "Pug", "Rottweiler", "English Setter", "Maltese", "Basset Hound", 
-"German Shorthaired Pointer", "Weimaraner", "Border Collie",
-"Shetland Sheepdog", "Dobermann", "Chesapeake Bay Retriever", "Bernese Mountain", 
-"Great Dane", "Rhodesian Ridgeback", "Siberian Husky", "Australian Shepherd", "Corgi", 
-"Boston Terrier", "Pomeranian", "Havanese", "English Mastiff" ]
+var puppyArray = [ "fabrador retriever", "german shepherd", "poodle", "chihuahua", 
+"golden retriever", "yorkie", "dachshund", "beagle", "boxer", 
+"schnauzer", "shih tzu", "bulldog", "akita", "cocker spaniel", "st bernard", 
+"bulldog", "pug", "rottweiler", "english setter", "maltese", "basset hound", 
+"german shorthaired pointer", "weimaraner", "border collie",
+"shetland sheepdog", "dobermann", "chesapeake bay retriever", "bernese mountain", 
+"great dane", "rhodesian ridgeback", "siberian husky", "australian shepherd", "corgi", 
+"boston terrier", "pomeranian", "havanese", "english mastiff" ]
+// Array for each name!//
+var 
+
 
 // Array of images? not sure if possible yet//
 
 var imageArray = [?]
 
-// 
+// If Else Example //
+
+if (true) {
+    alert("goo goo dolls");
+  }
+  else {
+    alert("vanilla ice");
+  }
+
+  if (false) {
+    alert("the neighborhood");
+  }
+  else {
+    alert("one direction");
+  }
+
+  if (("5" + "5") === "55") {
+    alert("tofu");
+  }
+  else {
+    alert("soybean");
+  }
 
 
 // Console loggs //
 
 console.log (puppyArray)
+
+// This is a LOOP //
+// for (var i= 0; 1 < puppyArray.length; i++) //
