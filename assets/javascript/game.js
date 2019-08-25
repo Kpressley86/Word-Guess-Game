637ab59baaf5c8b5867db2229fc5704e94fad93f
Kelly Pressley <kellypressley86@gmail.com>
2019-08-25 15:44:19 -0700

    fixed randomPuppy variable in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,9 @@
 //Start//
-var name = prompt("What is your name?");
-console.log("Hello" + name);
-
-var randomPuppy = getRandomPuppy();
+// var name = prompt("What is your name?");
+// console.log("Hello" + name);
 
 // FUNCTIONS //
 
-function getRandomPuppy() {
-  var puppyArray = puppyArray[Math.floor(Math.random() * puppyArray.length)];
-}
 
 function userGuess () {
 
@@ -22,62 +17,66 @@ function reset() {
 
 }
 
+// picking a random dog//
+// function getRandomPuppy() {
+//   return puppyArray[Math.floor(Math.random()*puppyArray.length)];
+// }
+
 // Array of dog names //
 
-var puppyArray = [ "labrador retriever", "german shepherd", "poodle", "chihuahua", 
+let puppyArray = [ "labrador retriever", "german shepherd", "poodle", "chihuahua", 
 "golden retriever", "yorkie", "dachshund", "beagle", "boxer", 
 "schnauzer", "shih tzu", "bulldog", "akita", "cocker spaniel", "st bernard", 
 "bulldog", "pug", "rottweiler", "english setter", "maltese", "basset hound", 
 "german shorthaired pointer", "weimaraner", "border collie",
 "shetland sheepdog", "dobermann", "chesapeake bay retriever", "bernese mountain", 
 "great dane", "rhodesian ridgeback", "siberian husky", "australian shepherd", "corgi", 
-"boston terrier", "pomeranian", "havanese", "english mastiff" ]''
-console.log(puppyArray);
+"boston terrier", "pomeranian", "havanese", "english mastiff" ];
 
-// FUNCTIONS //
+let randomPuppy = puppyArray[Math.floor(Math.random()*puppyArray.length)];
 
-// picking a random dog//
-function getRandomPuppy() {
-  var puppyArray = puppyArray[Math.floor(Math.random() * puppyArray.length)];
-}
+document.body.innerHTML = randomPuppy;
+
+console.log(randomPuppy);
+
 
 // Array for each name!//
-var answerArray = ["_","_"];
-u for (var i = 0; i < puppyArray.length; i++) {
- answerArray[i] = "_";
-}
-var remainingLetters = puppyArray.length;
+// var answerArray = ["_","_"];
+// u for (var i = 0; i < puppyArray.length; i++) {
+//  answerArray[i] = "_";
+// }
+// var remainingLetters = puppyArray.length;
 
 
 // Array of images? not sure if possible yet//
 
-var imageArray = [?]
+// var imageArray = [?]
 
 // If Else Example //
 
-if (true) {
-    alert("goo goo dolls");
-  }
-  else {
-    alert("vanilla ice");
-  }
+// if (true) {
+//     alert("goo goo dolls");
+//   }
+//   else {
+//     alert("vanilla ice");
+//   }
 
-  if (false) {
-    alert("the neighborhood");
-  }
-  else {
-    alert("one direction");
-  }
+//   if (false) {
+//     alert("the neighborhood");
+//   }
+//   else {
+//     alert("one direction");
+//   }
 
-  if (("5" + "5") === "55") {
-    alert("tofu");
-  }
-  else {
-    alert("soybean");
-  }
+//   if (("5" + "5") === "55") {
+//     alert("tofu");
+//   }
+//   else {
+//     alert("soybean");
+//   }
 
 
-// Console loggs //
+ // Console loggs //
 
 console.log (puppyArray)
 
