3732b4580185d691a74a941dd1dd442b3080951d
Kelly Pressley <kellypressley86@gmail.com>
2019-08-25 20:19:42 -0700

    added more js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 // var name = prompt("What is your name?");
 // console.log("Hello" + name);
 
-function displayGuess () {
+function displayGuess() {
 
 }
 
@@ -13,33 +13,41 @@ function reset() {
 
 // Array of dog names //
 
-let puppyArray = [ "labrador retriever", "german shepherd", "poodle", "chihuahua", 
-"golden retriever", "yorkie", "dachshund", "beagle", "boxer", 
-"schnauzer", "shih tzu", "bulldog", "akita", "cocker spaniel", "st bernard", 
-"bulldog", "pug", "rottweiler", "english setter", "maltese", "basset hound", 
-"german shorthaired pointer", "weimaraner", "border collie",
-"shetland sheepdog", "dobermann", "chesapeake bay retriever", "bernese mountain", 
-"great dane", "rhodesian ridgeback", "siberian husky", "australian shepherd", "corgi", 
-"boston terrier", "pomeranian", "havanese", "english mastiff" ];
-console.log (puppyArray)
+let puppyArray = ["labrador retriever", "german shepherd", "poodle", "chihuahua",
+  "golden retriever", "yorkie", "dachshund", "beagle", "boxer",
+  "schnauzer", "shih tzu", "bulldog", "akita", "cocker spaniel", "st bernard",
+  "bulldog", "pug", "rottweiler", "english setter", "maltese", "basset hound",
+  "german shorthaired pointer", "weimaraner", "border collie",
+  "shetland sheepdog", "dobermann", "chesapeake bay retriever", "bernese mountain",
+  "great dane", "rhodesian ridgeback", "siberian husky", "australian shepherd", "corgi",
+  "boston terrier", "pomeranian", "havanese", "english mastiff"];
+console.log(puppyArray)
 
 // picking a random dog//
 
-let randomPuppy = puppyArray[Math.floor(Math.random()*puppyArray.length)];
+let randomPuppy = puppyArray[Math.floor(Math.random() * puppyArray.length)];
 
 document.body.innerHTML = randomPuppy;
 
 console.log(randomPuppy);
 
-// Getting the users GUESS! //
 
-function userGuess () {
-document.getElementById("userGuess")
+// answer to randomPuppy? //
+
+var answerArray = [];
+for (var i = 0; i < randomPuppy.length; i++) {
+  answerArray[i] = "_";
 }
+var remainingLetters = randomPuppy.length;
+while (remainingLetters > 0)
+  for (var j = 0; j < randomPuppy.length; j++) {
+    if (randomPuppy[j] === guess) {
+      answerArray[j] = guess;
+      remainingLetters--;
+    }
+  }
 
-
-
-
-
-// This is a LOOP //
-// for (var i= 0; 1 < puppyArray.length; i++) //
+  // userGuess //
+  function userGuess() {
+    document.getElementById("guess").value;
+  }
